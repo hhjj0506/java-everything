99ce27610f08a5760bdec6263f9028a48aedd1c2
Hyeongjin Kim <hhjj0506@gmail.com>
2023-08-25 19:08:24 +0900

    P1934

---
+++ PS/P1934.java
@@ -0,0 +1,25 @@
+package PS;
+
+import java.util.Scanner;
+
+public class P1934 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int res = a * b / gcd(a, b); // euclidean algorithm
+            System.out.println(res);
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
